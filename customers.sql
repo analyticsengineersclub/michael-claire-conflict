a38fa4cc0efebca60fd591d62b4d2a1141eee151
mikekaminsky <kaminsky.michael@gmail.com>
2022-08-19 14:15:45 -0500

    Change style

--- customers.sql
+++ customers.sql
@@ -3,8 +3,7 @@ with customer_orders as (
           customer_id
         , count(*) as n_orders
         , min(created_at) as first_order_at
-
-    from `analytics-engineers-club.coffee_shop.orders` 
+    FROM `analytics-engineers-club.coffee_shop.orders` 
     group by 1
 )
 
